5752091ec1b907fbe623553f442d887f20de9851
Sebastian Tedesco <stedescoc@gmail.com>
2019-11-12 20:24:08 -0800

    did skeleton AND 'Flip One Card' in the this branch

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,11 @@
+$(document).ready(initializeApp);
 
+function initializeApp(){
+  var card = $(".card > .back");
+  card.on("click", handleCardClick);
+}
+
+function handleCardClick(event){
+  console.log("event parameter: ", event);
+  $(event.currentTarget).addClass("hidden");
+}
