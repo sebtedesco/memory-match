684eae90d51738e6a7936e43738d7526627c2c52
Sebastian Tedesco <stedescoc@gmail.com>
2019-11-13 11:06:05 -0800

    Started matching two cards

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,9 @@
 $(document).ready(initializeApp);
 
+var firstCardClicked = null;
+var secondCardClicked = null;
+var matches = null;
+
 function initializeApp(){
   var card = $(".card > .back");
   card.on("click", handleCardClick);
@@ -8,4 +12,8 @@ function initializeApp(){
 function handleCardClick(event){
   console.log("event parameter: ", event);
   $(event.currentTarget).addClass("hidden");
+  if(!firstCardClicked){
+    firstCardClicked = $(event.currentTarget);
+    console.log("firstCardClicked stuff: ", firstCardClicked);
+  }
 }
