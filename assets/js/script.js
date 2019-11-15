c35db5ad090c4457843cb36a38d40e08d9556e4e
Sebastian Tedesco <stedescoc@gmail.com>
2019-11-15 11:44:49 -0800

    this is several features in one! the feature in progress is the shuffle cards. all features before are done.

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,49 +2,114 @@ $(document).ready(initializeApp);
 
 var firstCardClicked = null;
 var secondCardClicked = null;
-var matches = null;
+var matches = 0;
 var maxMatches = 9;
+var attempts = 0;
+var gamesPlayed = 0;
+var winModal;
+var accuracyPercentage = 0;
+var canBeClicked = true;
 
 function initializeApp(){
   var card = $(".card > .back");
   card.on("click", handleCardClick);
+  $("div.reset-game-button").on("click", resetStats);
 }
+
 function handleCardClick(event){
-  // var frontCardUrl = $(".card .front").css("background-image");
+  if(!canBeClicked) {
+    return;
+  }
   console.log("event parameter: ", event);
   $(event.currentTarget).addClass("hidden");
+
   if(!firstCardClicked){
     firstCardClicked = $(event.currentTarget);
     console.log("firstCardClicked stuff: ", firstCardClicked);
+
   } else {
+    canBeClicked = false;
     secondCardClicked = $(event.currentTarget);
     console.log("SECOND card clicked: " + secondCardClicked);
+
     var firstCardBackground = firstCardClicked.prev().css('background-image');
     var secondCardBackground = secondCardClicked.prev().css('background-image');
+
     if(firstCardBackground === secondCardBackground){
       console.log("it's a match!");
       matches++;
+      attempts++;
+      displayStats();
       firstCardClicked = null;
       secondCardClicked = null;
+      canBeClicked = true;
+
         if(matches === maxMatches){
-          alert("You win!")
+          gamesPlayed++;
+          displayStats();
+          winModal = $("div.you-win-modal");
+          winModal.removeClass("hidden");
         }
     }else{
       setTimeout(function (){
         firstCardClicked.removeClass("hidden");
         secondCardClicked.removeClass("hidden");
+        firstCardClicked = 0;
+        secondCardClicked = 0;
+        gamesPlayed++;
+        attempts++;
+        displayStats();
         firstCardClicked = null;
         secondCardClicked = null;
+        canBeClicked = true;
        }, 1500);
     }
-
+  }
+}
+  function calculateAccuracy(){
+    accuracyPercentage = ((matches / attempts) * 100).toFixed(2) + "%";
+    return accuracyPercentage;
   }
 
+  function displayStats(){
+    $(".stats-box #attempts").text(attempts);
+    $(".stats-box #games-played").text(gamesPlayed);
+    $(".stats-box #accuracy").text(calculateAccuracy());
+  }
+
+  function resetStats(){
+    matches = 0;
+    attempts = 0;
+    gamesPlayed++;
+    displayStats();
+    $(".main div").removeClass("hidden");
+    winModal.addClass("hidden");
+  }
+
+  function shuffleCards(){
+    var arrOfCardUrls = ["html-logo", "html - logo", "gitHub-logo", "gitHub-logo", "css-logo",
+    "css-logo", "docker-logo", "docker-logo", "front js-logo", "front js-logo", "react-logo",
+    "react-logo", "mysql-logo", "mysql-logo", "node-logo", "node-logo", "php-logo", "php-logo"];
+
+    var randomIndex = Math.floor(Math.random() * 20);
+    var randomFrontCardLogoClass = arrOfCardUrls[randomIndex];
+    var frontSkelDiv = $("<div>").addClass("front");
+    var backCardDiv = $("<div>").addClass("back");
+    var rowDiv = $("<div>").addClass("row");
 
 
 
+    for(rowIndex = 0; rowIndex <3; rowIndex++){
+      rowDiv.append(cardDiv);
+    }
+    var arrayIndex = 0;
+    while(arrayIndex = <19){
 
-  // if (firstCardClicked.frontCardUrl === secondCardClicked.frontCardUrl){
-  //   console.log("cards match");
-  // }
-}
+    }
+
+
+
+    )
+
+
+  }
